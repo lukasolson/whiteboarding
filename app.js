cc2fdd3c82a1198958e7fb1b582eda1e2f067c5f
Lukas Olson <olson.lukas@gmail.com>
2013-11-08 16:16:21 -0700

    Change references to live service

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
 	new WhiteboardCanvas(
 		document.getElementById("whiteboard"),
-		io.connect("http://localhost:1113"),
+		io.connect("http://askullsoon.com:1113"),
 		window.location.hash.substring(1)
 	);
 })();
